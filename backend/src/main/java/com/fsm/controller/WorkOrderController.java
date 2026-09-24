d7d4131767f7f024d49025cc6121c7ad343dbd1e
Kunigiri Kavya <kunigirikavya@gmail.com>
2026-09-24 17:58:07 +0530

    feat: expose paginated work-order API with filtering

--- backend/src/main/java/com/fsm/controller/WorkOrderController.java
+++ backend/src/main/java/com/fsm/controller/WorkOrderController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Collectors;\nimport org.springframework.data.domain.Page;\nimport org.springframework.data.domain.Pageable;\nimport org.springframework.data.web.PageableDefault;\nimport org.springframework.data.domain.Sort;\nimport com.fsm.dto.PagedResponse;
 
 @RestController
 @RequestMapping("/api/work-orders")
